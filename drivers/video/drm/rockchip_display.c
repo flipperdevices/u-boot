15b09e6769fc5cd9ec91bae0bed96fc3c650465e
Damon Ding <damon.ding@rock-chips.com>
2025-07-22 14:36:22 +0800

    video/drm: display: Fix compile errors if CONFIG_ROCKCHIP_RESOURCE_IMAGE is disabled
    Some functions will not be used if CONFIG_ROCKCHIP_RESOURCE_IMAGE is
    disabled, such as bitmap_create(), bitmap_get_buffer() and so on.
    
    Therefore, add config macro CONFIG_ROCKCHIP_RESOURCE_IMAGE check for
    these functions to avoid complie errors like:
    
    ......
    drivers/video/drm/rockchip_display.c:1478:14: error: ‘bitmap_create’
    defined but not used [-Werror=unused-function]
     static void *bitmap_create(int width, int height, unsigned int state)
                  ^~~~~~~~~~~~~
    ......
    
    Change-Id: I156e88951769bb807c7479d2fdc2cb999cb76bc0
    Signed-off-by: Damon Ding <damon.ding@rock-chips.com>

--- drivers/video/drm/rockchip_display.c
+++ drivers/video/drm/rockchip_display.c
@@ -1475,6 +1475,7 @@ free_header:
 }
 #endif
 
+#ifdef CONFIG_ROCKCHIP_RESOURCE_IMAGE
 static void *bitmap_create(int width, int height, unsigned int state)
 {
 	/* Ensure a stupidly large bitmap is not created */
@@ -1589,6 +1590,7 @@ static void *rockchip_logo_rotate(struct logo_info *logo, void *src)
 
 	return dst_rotate;
 }
+#endif
 
 static int load_bmp_logo(struct logo_info *logo, const char *bmp_name)
 {
