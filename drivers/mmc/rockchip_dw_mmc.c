flipperdevices/u-boot

commit e6486ffb4951afb881d022bcef76ba8b6ce9d45a

mmc: rockchip_dw_mmc: rectify clk setting
The mmc controller has an internal div2, except rk3126c, rk3128, rk3036.

Signed-off-by: Ziyuan Xu <xzy.xu@rock-chips.com>
Change-Id: Ic9e6e05c560d732be210405ba429e3b8b326cbf9

drivers/mmc/rockchip_dw_mmc.c
@@ -82,7 +82,13 @@ static uint rockchip_dwmmc_get_mmc_clk(struct dwmci_host *host, uint freq)
 	if (mmc_card_ddr52(host->mmc) && host->mmc->bus_width == 8)
 		freq *= 2;
 
+#if defined(CONFIG_ROCKCHIP_RK3036) || \
+    defined(CONFIG_ROCKCHIP_RK3126) || \
+    defined(CONFIG_ROCKCHIP_RK3128)
 	ret = clk_set_rate(&priv->clk, freq);
+#else
+	ret = clk_set_rate(&priv->clk, freq * 2);
+#endif
 	if (ret < 0) {
 		debug("%s: err=%d\n", __func__, ret);
 		return 0;

commit 2f96689a87bd0928cdf165756847434b76799447

mmc: rockchip_dw_mmc: only set biu_clkx2 for rv1126b
Fixes: e6486ffb4951 ("mmc: rockchip_dw_mmc: rectify clk setting")
Change-Id: I248c802910b6d5d61d10c4452b0bdfe65d511257
Signed-off-by: Ziyuan Xu <xzy.xu@rock-chips.com>

drivers/mmc/rockchip_dw_mmc.c
@@ -82,12 +82,10 @@ static uint rockchip_dwmmc_get_mmc_clk(struct dwmci_host *host, uint freq)
 	if (mmc_card_ddr52(host->mmc) && host->mmc->bus_width == 8)
 		freq *= 2;
 
-#if defined(CONFIG_ROCKCHIP_RK3036) || \
-    defined(CONFIG_ROCKCHIP_RK3126) || \
-    defined(CONFIG_ROCKCHIP_RK3128)
-	ret = clk_set_rate(&priv->clk, freq);
-#else
+#if defined(CONFIG_ROCKCHIP_RV1126B)
 	ret = clk_set_rate(&priv->clk, freq * 2);
+#else
+	ret = clk_set_rate(&priv->clk, freq);
 #endif
 	if (ret < 0) {
 		debug("%s: err=%d\n", __func__, ret);