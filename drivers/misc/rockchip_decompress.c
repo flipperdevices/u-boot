flipperdevices/u-boot

commit 369ace90d82ae566e779799b84d1e4db411e42c6

misc: rockchip decompress: Enable IRQ when storage is spi-nand in SPL
1. If you need to auto trigger an interrupt to wake up rootfs
   unpack when kernel DECOM driver is initialized, you need to enable
   DECOM interrupt in SPL.
2. SPL may decompress multiple packages, and interrupts need to be
   cleared first to prevent from being triggered when it enters kernel
   without completing the decompression.

Signed-off-by: Xuhui Lin <xuhui.lin@rock-chips.com>
Change-Id: I784af2eab797b4dba5c8577518c414b19f596a2e

drivers/misc/rockchip_decompress.c
@@ -98,6 +98,7 @@ static int rockchip_decom_start(struct udevice *dev, void *buf)
 	struct decom_param *param = (struct decom_param *)buf;
 	unsigned int limit_lo = param->size_dst & 0xffffffff;
 	unsigned int limit_hi = param->size_dst >> 32;
+	u32 irq_status;
 
 #if CONFIG_IS_ENABLED(DM_RESET)
 	reset_assert(&priv->rst);
@@ -118,6 +119,11 @@ static int rockchip_decom_start(struct udevice *dev, void *buf)
 
 	priv->done = false;
 
+	irq_status = readl(priv->base + DECOM_ISR);
+	/* clear interrupts */
+	if (irq_status)
+		writel(irq_status, priv->base + DECOM_ISR);
+
 	if (param->mode == DECOM_LZ4)
 		writel(LZ4_CONT_CSUM_CHECK_EN |
 		       LZ4_HEAD_CSUM_CHECK_EN |
@@ -137,6 +143,8 @@ static int rockchip_decom_start(struct udevice *dev, void *buf)
 	writel(limit_lo, priv->base + DECOM_LMTSL);
 	writel(limit_hi, priv->base + DECOM_LMTSH);
 
+	if (param->flags && DCOMP_FLG_IRQ_ONESHOT)
+		writel(DECOM_INT_MASK, priv->base + DECOM_IEN);
 	writel(DECOM_ENABLE, priv->base + DECOM_ENR);
 
 	priv->idle_check_once = true;

commit b35821ad40b35c23d66b2b59ff68fcff85733bd2

rockchip: decompress: Fix DCOMP_FLG_IRQ_ONESHOT support
Fixes: 369ace90d82 ("misc: rockchip decompress: Enable IRQ when storage is spi-nand in SPL")
Change-Id: I13b48fbb3f462ca94a72e8773cbb41286197ac84
Signed-off-by: Xuhui Lin <xuhui.lin@rock-chips.com>

drivers/misc/rockchip_decompress.c
@@ -146,8 +146,10 @@ static int rockchip_decom_start(struct udevice *dev, void *buf)
 	writel(limit_lo, priv->base + DECOM_LMTSL);
 	writel(limit_hi, priv->base + DECOM_LMTSH);
 
-	if (param->flags && DCOMP_FLG_IRQ_ONESHOT)
+	if (param->flags & DCOMP_FLG_IRQ_ONESHOT) {
 		writel(DECOM_INT_MASK, priv->base + DECOM_IEN);
+		param->flags &= ~DCOMP_FLG_IRQ_ONESHOT;
+	}
 	writel(DECOM_ENABLE, priv->base + DECOM_ENR);
 
 	priv->idle_check_once = true;