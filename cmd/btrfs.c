1f49224abe68a7ab4917bfa69375767274f9217b
Anton Burticica <mouse@ya.ru>
2026-01-20 09:54:07 +0200

    cmd: btrfs: Add btrls and btrload commands for BTRFS filesystem access
    Add convenience commands for listing and loading files from BTRFS
    filesystems, consistent with other filesystem command naming:
    
      btrls <interface> [<dev[:part]>] [directory]
        - List files in a directory (default /) on a BTRFS filesystem
    
      btrload <interface> [<dev[:part]>] <addr> <filename> [bytes [pos]]
        - Load a binary file from a BTRFS filesystem to memory
    
    These commands mirror the functionality of extls/extload, fatls/fatload,
    etc., providing a familiar interface for BTRFS users. They wrap the
    existing do_ls() and do_load() generic implementations with FS_TYPE_BTRFS.
    
    Change-Id: 45309060-a5ed-41d0-9ecd-45d290589078
    Signed-off-by: Anton Burticica <mouse@ya.ru>

--- cmd/btrfs.c
+++ cmd/btrfs.c
@@ -19,8 +19,34 @@ int do_btrsubvol(struct cmd_tbl *cmdtp, int flag, int argc, char *const argv[])
 	return 0;
 }
 
+static int do_btr_ls(struct cmd_tbl *cmdtp, int flag, int argc, char *const argv[])
+{
+	return do_ls(cmdtp, flag, argc, argv, FS_TYPE_BTRFS);
+}
+
+static int do_btr_load(struct cmd_tbl *cmdtp, int flag, int argc, char *const argv[])
+{
+	return do_load(cmdtp, flag, argc, argv, FS_TYPE_BTRFS);
+}
+
 U_BOOT_CMD(btrsubvol, 3, 1, do_btrsubvol,
 	"list subvolumes of a BTRFS filesystem",
 	"<interface> <dev[:part]>\n"
 	"     - List subvolumes of a BTRFS filesystem."
+);
+
+U_BOOT_CMD(
+	btrls, 4, 1, do_btr_ls,
+	"list files in a directory (default /) on a BTRFS filesystem",
+	"<interface> [<dev[:part]>] [directory]\n"
+	"    - list files from 'dev' on 'interface' in 'directory'"
+);
+
+U_BOOT_CMD(
+	btrload, 7, 0, do_btr_load,
+	"load binary file from a BTRFS filesystem",
+	"<interface> [<dev[:part]>] <addr> <filename> [bytes [pos]]\n"
+	"    - load file 'filename' from 'dev' on 'interface' to 'addr'\n"
+	"      'pos' is the file offset to start from; if 'bytes' is 0 or\n"
+	"      omitted, the whole file is read. Sets env fileaddr/filesize."
 );
