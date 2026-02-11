217c2cc68e6330fabbddf31391b6d105b370d3f6
Alexey Charkov <alchark@flipper.net>
2026-02-11 11:53:16 +0400

    board: rockchip: Add Flipper One
    Flipper One is a portable multi-tool device with a built-in display and
    various interfaces, built around the Rockchip RK3576 SoC.
    
    Add a new defconfig file for building U-Boot for this device.
    
    Signed-off-by: Alexey Charkov <alchark@flipper.net>

---
+++ include/configs/flipper-one-rk3576.h
@@ -0,0 +1,17 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Configuration for the Flipper One (RK3576)
+ *
+ * Copyright (c) 2026 Flipper FZCO
+ */
+
+#ifndef __CONFIG_FLIPPER_ONE_RK3576_H
+#define __CONFIG_FLIPPER_ONE_RK3576_H
+
+#define ROCKCHIP_DEVICE_SETTINGS \
+	"stdout=serial,vidconsole\0" \
+	"stderr=serial,vidconsole\0"
+
+#include "rk3576_common.h"
+
+#endif /* __CONFIG_FLIPPER_ONE_RK3576_H */
