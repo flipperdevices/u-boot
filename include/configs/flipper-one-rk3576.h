87b11ca239e63ace24ea76b6a67d92a6c1d5c07d
Alexey Charkov <alchark@flipper.net>
2026-05-06 17:00:12 +0400

    Enable graphical boot menu with button navigation on Flipper One
    Signed-off-by: Alexey Charkov <alchark@flipper.net>

--- include/configs/flipper-one-rk3576.h
+++ include/configs/flipper-one-rk3576.h
@@ -9,8 +9,11 @@
 #define __CONFIG_FLIPPER_ONE_RK3576_H
 
 #define ROCKCHIP_DEVICE_SETTINGS \
+	"stdin=serial,flipper-one-btns\0" \
 	"stdout=serial,vidconsole\0" \
 	"stderr=serial,vidconsole\0"
+
+#define BOOT_TARGETS "mmc0 scsi"
 
 #include "rk3576_common.h"
 
