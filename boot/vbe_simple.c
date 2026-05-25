ed3118a3463edab8f55c96be6ffb014f4b52b27a
Alexey Charkov <alchark@flipper.net>
2026-05-25 19:13:04 +0400

    bootstd: add seq parameter to bootmeth read_bootflow op
    Add an integer 'seq' parameter to the bootmeth read_bootflow operation
    to pave way for bootmeths returning more than one entry per tuple
    of (method, partition, bootdev). If available entries are exhausted
    the bootmeths return -ENOENT, which terminates the iteration.
    
    No functional change.
    
    Signed-off-by: Alexey Charkov <alchark@flipper.net>

--- boot/vbe_simple.c
+++ boot/vbe_simple.c
@@ -81,13 +81,17 @@ static int vbe_simple_get_state_desc(struct udevice *dev, char *buf,
 	return 0;
 }
 
-static int vbe_simple_read_bootflow(struct udevice *dev, struct bootflow *bflow)
+static int vbe_simple_read_bootflow(struct udevice *dev, struct bootflow *bflow,
+				    int seq)
 {
 	int ret;
 
+	if (seq)
+		return -ENOENT;
+
 	if (CONFIG_IS_ENABLED(BOOTMETH_VBE_SIMPLE_FW)) {
 		if (vbe_phase() == VBE_PHASE_FIRMWARE) {
-			ret = vbe_simple_read_bootflow_fw(dev, bflow);
+			ret = vbe_simple_read_bootflow_fw(dev, bflow, seq);
 			if (ret)
 				return log_msg_ret("fw", ret);
 			return 0;
