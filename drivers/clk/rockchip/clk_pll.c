57fc192d5ef637e2b29b82c43d4b94a6aad479dd
Alexey Charkov <alchark@flipper.net>
2026-07-13 17:47:35 +0400

    clk: rockchip: pll: let rockchip_rk3588_pll_k_get update m directly
    Selecting the next integer multiplier m is coupled to setting a negative
    fractional coefficient k. The current code checks for negative k in two
    separate places, which is error-prone.
    
    Let rockchip_rk3588_pll_k_get update m directly, to make it the single
    source of truth for the final value of the integer multiplier m, which
    also reduces the number of scattered conditional branches in the code.
    
    Signed-off-by: Alexey Charkov <alchark@flipper.net>

--- drivers/clk/rockchip/clk_pll.c
+++ drivers/clk/rockchip/clk_pll.c
@@ -168,17 +168,18 @@ rockchip_pll_clk_set_by_auto(ulong fin_hz,
 }
 
 static s16
-rockchip_rk3588_pll_k_get(u32 m, u32 p, u32 s, u64 fin_hz, u64 fvco)
+rockchip_rk3588_pll_k_get(u32 *m, u32 p, u32 s, u64 fin_hz, u64 fvco)
 {
 	u64 fref, ffrac;
 	int k;
 
 	fref = fin_hz / p;
-	ffrac = fvco - (m * fref);
+	ffrac = fvco - (*m) * fref;
 	k = ffrac * 65536 / fref;
 	if (k > 32767) {
+		*m += 1;
 		fref = fin_hz / p;
-		ffrac = ((m + 1) * fref) - fvco;
+		ffrac = (*m) * fref - fvco;
 		/*
 		 * Round up to avoid overshooting requested rate for negative k
 		 */
@@ -202,7 +203,7 @@ rockchip_rk3588_pll_frac_by_auto(unsigned long fin_hz, unsigned long fout_hz)
 			for (m = 64; m <= 1023; m++) {
 				if ((fvco >= m * fin_hz / p) &&
 				    (fvco < (m + 1) * fin_hz / p)) {
-					k = rockchip_rk3588_pll_k_get(m, p, s,
+					k = rockchip_rk3588_pll_k_get(&m, p, s,
 								      fin_hz,
 								      fvco);
 					if (!k)
@@ -210,10 +211,7 @@ rockchip_rk3588_pll_frac_by_auto(unsigned long fin_hz, unsigned long fout_hz)
 					rate_table->p = p;
 					rate_table->s = s;
 					rate_table->k = k;
-					if (k > 32767)
-						rate_table->m = m + 1;
-					else
-						rate_table->m = m;
+					rate_table->m = m;
 					return rate_table;
 				}
 			}
