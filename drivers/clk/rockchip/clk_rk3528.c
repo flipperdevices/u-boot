07ac49dc7bc22dfaf811600dd44956c5f6a0356e
Jonas Karlman <jonas@kwiboo.se>
2026-08-04 00:26:42 +0000

    clk: rockchip: rk3528: Always init clocks in SPL build
    The core PLLs and clocks are only initialized in SPL on RK3528.
    
    Ensure clock driver is always probed after bind for SPL builds to
    initialize core PLLs and clocks in SPL phase on RK3528.
    
    Signed-off-by: Jonas Karlman <jonas@kwiboo.se>

--- drivers/clk/rockchip/clk_rk3528.c
+++ drivers/clk/rockchip/clk_rk3528.c
@@ -1714,6 +1714,10 @@ static int rk3528_clk_bind(struct udevice *dev)
 	struct sysreset_reg *priv;
 	int ret;
 
+	/* Ensure clocks are initialized after bind in SPL build */
+	if (IS_ENABLED(CONFIG_SPL_BUILD))
+		dev_or_flags(dev, DM_FLAG_PROBE_AFTER_BIND);
+
 	/* The reset driver does not have a device node, so bind it here */
 	ret = device_bind_driver(dev, "rockchip_sysreset", "sysreset",
 				 &sys_child);
